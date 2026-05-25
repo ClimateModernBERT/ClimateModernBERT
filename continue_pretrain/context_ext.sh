973eb9df7a8cd701d02dacbed3f3c5f86a6cfc0e
armsp <shantamdps@gmail.com>
2026-05-25 22:28:40 +0200

    adding the composer training scripts with slurm job submissions

---
+++ continue_pretrain/context_ext.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --mem=65536
+#SBATCH --time=168:00:00
+#SBATCH --gpus=A100:4
+#SBATCH -o logs/%j.log
+#SBATCH -e logs/%j.err
+module load dev2025a multigpu cuda/12.6.3
+
+export HF_HOME="/scratch/xxx/"
+export TRITON_CACHE_DIR="/scratch/xxx/tritoncache"
+export TRITON_HOME="/scratch/xxx/tritoncache"
+export VLLM_CACHE_ROOT="/scratch/xxx/vllmcache"
+
+source .venv/bin/activate
+
+WANDB__SERVICE_WAIT=300 composer main.py yamls/modernbert/modernbert-base-context-extension.yaml 
+
+
---
+++ continue_pretrain/lr_decay.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --mem=65536
+#SBATCH --time=24:00:00
+#SBATCH --gpus=A100:4
+#SBATCH -o logs/%j.log
+#SBATCH -e logs/%j.err
+module load dev2025a multigpu cuda/12.6.3
+
+
+export HF_HOME="/scratch/xxx/"
+export TRITON_CACHE_DIR="/scratch/xxx/tritoncache"
+export TRITON_HOME="/scratch/xxx/tritoncache"
+export VLLM_CACHE_ROOT="/scratch/xxx/vllmcache"
+
+source .venv/bin/activate
+
+
+WANDB__SERVICE_WAIT=300 composer main.py yamls/modernbert/modernbert-base-learning-rate-decay.yaml 
+
+
